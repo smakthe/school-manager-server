790ceb86c450400fafa4cf9a310c27f6481bf3c1
smakthe <scmakra99@gmail.com>
2026-04-14 21:54:16 +0530

    Implemented centralized pagy interception and add student count and teacher details to classroom serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,27 @@ class ApplicationController < ActionController::API
   include Pagy::Backend
 
   before_action :authenticate_request
+  # We removed the before_action :map_jsonapi_page_param because we are intercepting it directly below!
 
   attr_reader :current_user
+
+  # --- THE ULTIMATE PAGY INTERCEPTOR ---
+  def pagy(collection, vars = {})
+    # Safely extract the page whether the request is ?page[number]=2 or standard ?page=2
+    page_number = if params[:page].is_a?(Hash) || params[:page].is_a?(ActionController::Parameters)
+                    params.dig(:page, :number)
+                  else
+                    params[:page]
+                  end || 1
+
+    # Force the variables into Pagy before it does its own processing
+    vars[:page]  ||= page_number
+    vars[:items] ||= 20 # For Pagy v5/v6 compatibility
+    vars[:limit] ||= 20 # For Pagy v7/v8+ compatibility
+    
+    super(collection, vars)
+  end
+  # -------------------------------------
 
   private
 
--- app/serializers/classroom_serializer.rb
+++ app/serializers/classroom_serializer.rb
@@ -1,4 +1,20 @@
 class ClassroomSerializer
   include JSONAPI::Serializer
   attributes :school_id, :academic_year_id, :class_teacher_id, :grade, :section, :display_name
+
+  # Calculate the total number of students enrolled in this classroom
+  attribute :students_count do |object|
+    object.students.count
+  end
+
+  # Nest the basic teacher details if a class teacher is assigned
+  attribute :class_teacher do |object|
+    if object.class_teacher
+      {
+        id: object.class_teacher.id,
+        name: object.class_teacher.name,
+        employee_code: object.class_teacher.employee_code
+      }
+    end
+  end
 end
